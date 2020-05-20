919853f7cabfccd6680a8e7b23653ce795ed7398
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-20 20:05:14 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow(color)
-  puts "R: #{color}, O: #{color}, Y: #{color}, G: #{color}, B: #{color}, I: #{color}, V: #{color}"
+def display_rainbow(colors)
+  puts "R: #{colors}, O: #{colors}, Y: #{colors}, G: #{colors}, B: #{colors}, I: #{colors}, V: #{colors}"
 end
